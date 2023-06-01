766b3c14d237157a64411f756e4734aa1e94e2ec
supermii2 <alvis.ng15@gmail.com>
2023-06-01 20:24:39 +0800

    test on anagram

--- gctools/anagram-solver.js
+++ gctools/anagram-solver.js
@@ -1,6 +1,6 @@
 function runAnagramCheck() {
     var regexPattern = /[^A-Za-z0-9]/g;
-    const chars = "abcdefghijklmnopqrstuvwxyz1234567890";  
+    const chars = "abcdefghijklmnopqrstuvwxyz1234567890";
     const input = document.getElementById("input").value.replace(regexPattern, "").toLowerCase();
     const n = document.getElementById("input_n").value;
     var names = []
@@ -9,85 +9,87 @@ function runAnagramCheck() {
     if (document.getElementById("pokemon").checked) names.push('pokemon.json');
     //if (document.getElementById("moves").checked) names.push('moves.json');
     //if (document.getElementById("abilities").checked) names.push('abilities.json');
-    
+
     document.getElementById("output").innerHTML = "";
 
     const requests = names.map(x => fetch(x));
     Promise.all(requests)
-    .then(x => x.map(y => y.json()))
-    .then(x => x.flat())
-    .then(x => stringToObject(x))
-    .then(x => anagramCheck(stringToObject(input), x, n))
-    .then(x => {
-      var current = document.getElementById("output").innerHTML;
-      var nextOne = `${current} ${input} is an anagram of ${x.join(" and ")}.\n`;
-      document.getElementById("output").innerHTML = nextOne;
-    });
+        .then(x => x.map(y => y.json()))
+        .then(x => x.flat())
+        .then(x => {
+            document.getElementById("output").innerHTML = x
+        });
 
-    if (document.getElementById("output").innerHTML == "") {
-      document.getElementById("output").innerHTML = "No anagrams found."
-    }
+
+
+
+
+
+
+
+
+
 
 
     function stringToObject(s) {
-      var returnObj = {};
-  
-  
-      for (let index=0; index < chars.length; index++) {
-      returnObj[chars[index]] = 0;
-      }
-  
-      for (let index = 0; index < s.length; index++) {
-      returnObj[s[index]]++;
-      }
-      returnObj['word'] = s;
-      return returnObj;
-  }
-  
-  function checkIfLessThan(bigArr, smallArr) {
-      for (let index=0; index < chars.length; index++) {
-      if (bigArr[chars[index]] < smallArr[chars[index]]) {
-          return false;
-      } else {
-          return true;
-      }
-      }
-  }
-  
-  function removeFrom(bigArr, smallArr) {
-      var newObj = {};
-      for (let index=0; index < chars.length; index++) {
-      newObj[chars[index]] = bigArr[chars[index]] - smallArr[chars[index]];
-      }
-      return newObj;
-  }
-  
-  function isEmpty(stringObj) {
-      for (let index=0; index < chars.length; index++) {
-          if (stringObj[chars[index]] != 0) {
-              return false;
-          }
-      }
-      return true;
-  }
-  
-  function anagramCheck(stringObj, words, n) {
-      if (n == 0 && isEmpty(stringObj)) {
-          return [[]];
-      } else if (n == 0) {
-          return [];
-      } else {
-          var answers = [];
-          for (const word of words) {
-              if (checkIfLessThan(stringObj, word)) {
-                  var doIt = anagramCheck(removeFrom(stringObj, word), words, n - 1);
-                  doIt.map(x => x.push(word.word));
-                  for (item of doIt) {
-                      answers.push(item);
-                  }
-              }
-          }
-          return answers;
-      }
-  }
-  }
+        var returnObj = {};
+
+
+        for (let index = 0; index < chars.length; index++) {
+            returnObj[chars[index]] = 0;
+        }
+
+        for (let index = 0; index < s.length; index++) {
+            returnObj[s[index]]++;
+        }
+        returnObj['word'] = s;
+        return returnObj;
+    }
+
+    function checkIfLessThan(bigArr, smallArr) {
+        for (let index = 0; index < chars.length; index++) {
+            if (bigArr[chars[index]] < smallArr[chars[index]]) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    function removeFrom(bigArr, smallArr) {
+        var newObj = {};
+        for (let index = 0; index < chars.length; index++) {
+            newObj[chars[index]] = bigArr[chars[index]] - smallArr[chars[index]];
+        }
+        return newObj;
+    }
+
+    function isEmpty(stringObj) {
+        for (let index = 0; index < chars.length; index++) {
+            if (stringObj[chars[index]] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function anagramCheck(stringObj, words, n) {
+        if (n == 0 && isEmpty(stringObj)) {
+            return [[]];
+        } else if (n == 0) {
+            return [];
+        } else {
+            var answers = [];
+            for (const word of words) {
+                if (checkIfLessThan(stringObj, word)) {
+                    var doIt = anagramCheck(removeFrom(stringObj, word), words, n - 1);
+                    doIt.map(x => x.push(word.word));
+                    for (item of doIt) {
+                        answers.push(item);
+                    }
+                }
+            }
+            return answers;
+        }
+    }
+}
